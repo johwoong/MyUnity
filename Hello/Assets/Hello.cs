896de7920e995eb3e2756d76c3885c4680164f77
jhwong1234@gmail.com <jhwong1234@tukorea.ac.kr>
2024-11-11 00:43:32 +0900

    깃 강의를 위한 Unity/Hello 프로젝트 등록

--- Hello/Assets/Hello.cs
+++ Hello/Assets/Hello.cs
@@ -7,7 +7,7 @@ public class Hello : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello ¼öÁ¤!!");
     }
 
     // Update is called once per frame
